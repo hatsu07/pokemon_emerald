41fcbeca025ffceee146885962b7021afccb5793
hatsu07 <hay07ko@gmail.com>
2026-08-03 22:15:02 +0900

    analyze Battle Dome arrow animation data

--- asm/battle_dome.s
+++ asm/battle_dome.s
@@ -11286,7 +11286,7 @@ _08194092:
 _0819409C: .4byte gBattleDomeTourneyTreeButtonSheets
 _081940A0: .4byte 0x085D5E8D
 _081940A4: .4byte gBattleDomeTourneyTreePokeballSpriteTemplate
-_081940A8: .4byte 0x085D5A84
+_081940A8: .4byte gBattleDomeTourneyTreeCloseButtonSpriteTemplate
 _081940AC: .4byte 0x00007F40
 _081940B0:
 	add r0, sp, #4
